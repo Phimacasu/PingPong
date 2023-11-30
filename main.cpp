f33615b60268804b82b50d94b1af493fc02da641
soller26 <151714156+soller26@users.noreply.github.com>
2023-11-30 16:35:12 +0800

    Update main.cpp
    ball and paddle animation
    keyboard function
    ball reset

--- main.cpp
+++ main.cpp
@@ -1,23 +1,142 @@
 // main.cpp
-#include <GL/glut.h>
+#include <glut.h>
 #include "Game.h"
+#include <cmath>
+#include <ctime>
 
 Game game;
 
-void display() {
-    game.run();
-}
+float radius = 0.05f;
+float posX = 0.0f;
+float posY = 0.0f;
+float speedX = 0.02f;
+float speedY = 0.015f;
 
-void reshape(int w, int h) {
-    // Reshape code
-}
+float Width = 0.05f;
+float Height = 0.25f;
+float leftPadX = -1.0f;
+float leftPadY = 0.0f;
+float rightPadX = 0.94f;
+float rightPadY = 0.0f;
+float padSpeed = 0.035f;
+
+bool reset = false;
+int countdown = 60; 
 
 void keyboard(unsigned char key, int x, int y) {
-    game.handleKeyboard(key, x, y);
+    switch (key) {
+    case 'w':
+        leftPadY += padSpeed;
+        break;
+    case 's':
+        leftPadY -= padSpeed;
+        break;
+    }
 }
 
-void specialKeys(int key, int x, int y) {
-    game.handleSpecialKeys(key, x, y);
+void keycode(int key, int x, int y) {
+    if (key == GLUT_KEY_UP)
+        rightPadY += padSpeed;
+
+    if (key == GLUT_KEY_DOWN)
+        rightPadY -= padSpeed;
+}
+
+void update(int value) {
+    if (reset) {
+        countdown--;
+        if (countdown <= 0) {
+            reset = false;
+            countdown = 60;
+        }
+    }
+    else {
+        posX += speedX;
+        posY += speedY;
+
+ 
+        if (posX > 1.0f - radius || posX < -1.0f + radius) {
+            speedX = -speedX;
+        }
+
+        if (posY > 1.0f - radius || posY < -1.0f + radius) {
+            speedY = -speedY;
+        }
+
+ 
+        if (posX - radius < leftPadX + Width &&
+            posX + radius > leftPadX &&
+            posY - radius < leftPadY &&
+            posY + radius > leftPadY - Height) {
+            speedX = -speedX;
+        }
+
+
+        if (posX + radius > rightPadX &&
+            posX - radius < rightPadX + Width &&
+            posY - radius < rightPadY &&
+            posY + radius > rightPadY - Height) {
+            speedX = -speedX;
+        }
+
+ 
+        if (posX > 0.95f || posX < -0.95f) {
+            reset = true;
+            posX = 0.0f;
+            posY = 0.0f;
+        }
+    }
+
+    glutTimerFunc(16, update, 0);
+    glutPostRedisplay();
+}
+
+void paddle() {
+    glBegin(GL_QUADS);
+    glColor3f(1.0f, 1.0f, 1.0f);
+    glVertex2f(leftPadX, leftPadY);
+    glColor3f(1.0f, 0.0f, 1.0f);
+    glVertex2f(leftPadX + Width, leftPadY);
+    glColor3f(1.0f, 1.0f, 0.0f);
+    glVertex2f(leftPadX + Width, leftPadY - Height);
+    glColor3f(0.0f, 1.0f, 1.0f);
+    glVertex2f(leftPadX, leftPadY - Height);
+    glEnd();
+
+    glBegin(GL_QUADS);
+    glColor3f(1.0f, 1.0f, 1.0f);
+    glVertex2f(rightPadX, rightPadY);
+    glColor3f(1.0f, 0.0f, 1.0f);
+    glVertex2f(rightPadX + Width, rightPadY);
+    glColor3f(1.0f, 1.0f, 0.0f);
+    glVertex2f(rightPadX + Width, rightPadY - Height);
+    glColor3f(0.0f, 1.0f, 1.0f);
+    glVertex2f(rightPadX, rightPadY - Height);
+    glEnd();
+}
+
+void display() {
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glColor3f(1.0f, 0.5f, 0.0f);
+    glBegin(GL_TRIANGLE_FAN);
+    for (int i = 0; i <= 360; i++) {
+        float angle = 2.0f * 3.14f * i / 360.0f;
+        float x = posX + radius * std::cos(angle);
+        float y = posY + radius * std::sin(angle);
+        glVertex2f(x, y);
+    }
+    glEnd();
+
+    paddle();
+
+    glutSwapBuffers();
+}
+
+void init() {
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+    gluOrtho2D(-1.0, 1.0, -1.0, 1.0);
+    glutTimerFunc(16, update, 0);
 }
 
 int main(int argc, char** argv) {
@@ -27,7 +146,7 @@ int main(int argc, char** argv) {
     glutDisplayFunc(display);
     glutReshapeFunc(reshape);
     glutKeyboardFunc(keyboard);
-    glutSpecialFunc(specialKeys);
+    glutSpecialFunc(keycode);
     glutMainLoop();
     return 0;
 }
